2709adda9cb771ffe2042438791042a99975a9ac
Gregor <gregor@martynus.net>
2016-05-02 13:44:31 -0400

    test: remove obsolete test for modp
    re: https://github.com/calvinmetcalf/crypto-pouch/pull/29#discussion_r61692465

--- test.js
+++ test.js
@@ -93,30 +93,6 @@ test('ignore: _attachments', function (t) {
   })
   .catch(t.error);
 })
-test('modp: "modp5", ignore: "_attachments"', function (t) {
-  t.plan(1);
-  var dbName = 'seven';
-  var db = new PouchDB(dbName, {db: memdown});
-  db.crypto('password', {modp: 'modp5', ignore: '_attachments'})
-  db.put({
-    _id: 'id-12345678',
-    _attachments: {
-      'att.txt': {
-        content_type: 'text/plain',
-        data: 'TGVnZW5kYXJ5IGhlYXJ0cywgdGVhciB1cyBhbGwgYXBhcnQKTWFrZS' +
-              'BvdXIgZW1vdGlvbnMgYmxlZWQsIGNyeWluZyBvdXQgaW4gbmVlZA=='
-      }
-    }
-  }).then(function () {
-    return db.get('id-12345678', {
-      attachments: true,
-      binary: true
-    });
-  }).then(function (doc) {
-    t.ok(Buffer.isBuffer(doc._attachments['att.txt'].data), 'returns _attachments as Buffers');
-  })
-  .catch(t.error);
-})
 test('throws error when document has attachments', function (t) {
   t.plan(1);
   var dbName = 'eight';
