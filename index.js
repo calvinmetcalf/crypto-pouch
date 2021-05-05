a9bb455797b6a6664d6f28c25415950433c1cf20
Diana Thayer <garbados@gmail.com>
2021-05-04 19:50:28 -0700

    chore: use garbados-crypt for crypto

--- index.js
+++ index.js
@@ -1,41 +1,7 @@
-const { decodeUTF8, encodeUTF8, encodeBase64, decodeBase64 } = require('tweetnacl-util')
-const { secretbox, hash, randomBytes } = require('tweetnacl')
+const Crypt = require('garbados-crypt')
 const { transform } = require('transform-pouch')
 
-const IGNORE = ['_id', '_rev', '_deleted']
-
-class Crypt {
-  constructor (password) {
-    if (!password) { throw new Error('A password is required for encryption or decryption.') }
-    this._key = hash(decodeUTF8(password)).slice(0, secretbox.keyLength)
-  }
-
-  async encrypt (plaintext) {
-    const nonce = randomBytes(secretbox.nonceLength)
-    const messageUint8 = decodeUTF8(plaintext)
-    const box = secretbox(messageUint8, nonce, this._key)
-    const fullMessage = new Uint8Array(nonce.length + box.length)
-    fullMessage.set(nonce)
-    fullMessage.set(box, nonce.length)
-    const base64FullMessage = encodeBase64(fullMessage)
-    return base64FullMessage
-  }
-
-  async decrypt (messageWithNonce) {
-    const messageWithNonceAsUint8Array = decodeBase64(messageWithNonce)
-    const nonce = messageWithNonceAsUint8Array.slice(0, secretbox.nonceLength)
-    const message = messageWithNonceAsUint8Array.slice(
-      secretbox.nonceLength,
-      messageWithNonce.length
-    )
-    const decrypted = secretbox.open(message, nonce, this._key)
-    if (!decrypted) {
-      throw new Error('Could not decrypt!')
-    } else {
-      return encodeUTF8(decrypted)
-    }
-  }
-}
+const IGNORE = ['_id', '_rev', '_deleted', '_conflicts']
 
 module.exports = {
   transform,
